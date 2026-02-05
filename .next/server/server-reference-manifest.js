13ef93e493976a98653a427a28d5a70db071b79f
Arunkavi <arunkavi@fipsar.com>
2026-02-05 21:01:31 +0530

    Revert "initial commit"
    This reverts commit 7c7ce66b603b2a231126e4030872a3777115f341.

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3R3Tw/0c3vB3vtEn1juoOc6aHBAL+4h9OT19YQB12Rc=\"\n}"
--- src/components/KPIBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function KPIBar() {
-  return (
-    <div className="bg-blue-600 text-white rounded-lg p-6 mb-4" style={{padding:"50px"}}>
-      KPIs (Comparison vs Time Period)
-    </div>
-  );
-}
