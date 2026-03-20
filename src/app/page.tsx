e055719bab7218bceda22205ed1900851dedc1e7
Arunkavi <arunkavi@fipsar.com>
2026-03-20 05:10:17 -0400

    Update page.tsx -- Revert Qlik Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 'use client';
-// import Sidebar from '../components/Sidebar';
-// import KPIBar from '../components/KPIBar';
-// import PageTabs from '../components/PageTabs';
-// import Sections from '../components/Sections';
-// import YearSelectionListener from '../components/YearSelctionListener';
-import   Design  from "../components/TechDasboardDesign.jsx";
+import Sidebar from '../components/Sidebar';
+import KPIBar from '../components/KPIBar';
+import PageTabs from '../components/PageTabs';
+import Sections from '../components/Sections';
+import YearSelectionListener from '../components/YearSelctionListener';
+// import   Design  from "../components/TechDasboardDesign.jsx";
 
 export default function Page() {
   return (
     <div className="flex min-h-[calc(100vh-64px)]">
       {/* <YearSelectionListener /> */}
-        {/* <Sidebar />  */}
+         <Sidebar />  
       <main className="flex-1 p-6 overflow-y-auto">
-        {/* <KPIBar /> */}
+        <KPIBar /> 
         {/* <YearSelectionListener /> */}
-        {/* <PageTabs /> */}
-        {/* <Sections />   */}
-        < Design/>
+         <PageTabs /> 
+         <Sections />  
+        {/* < Design/> */}
       </main>
     </div>
   );
